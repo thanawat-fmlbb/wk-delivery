b071fe6c216a24dbf6f06feca64f05f94c17f73c
thanawinboon <pokepat27@gmail.com>
2023-12-08 22:27:25 +0700

    tracer

--- src/tasks.py
+++ src/tasks.py
@@ -1,3 +1,7 @@
+import celery
+from opentelemetry import propagate, trace
+from opentelemetry.trace.propagation.tracecontext import TraceContextTextMapPropagator
+
 from celery.exceptions import SoftTimeLimitExceeded
 from .database.models import create_delivery, set_delivery_status, StatusEnum
 from src import app, result_collector
@@ -10,57 +14,73 @@ RESULT_TASK_NAME = "wk-irs.tasks.send_result"
     name='wk-delivery.tasks.make_delivery'
 )
 def make_delivery(**kwargs):
-    main_id = kwargs.get('main_id')
-    user_id = kwargs.get('user_id')
-    user_address = kwargs.get('user_address')
-    
-    success = True
-    try:
-        # backdoor for testing timeout
-        if user_address == "timeout":
-            from time import sleep
-            sleep(40)
+    celery.current_task.request.headers.get("traceparent")
+    tracer = trace.get_tracer(__name__)
+    ctx = propagate.extract(celery.current_task.request.headers)
+    with tracer.start_as_current_span("create_order", context=ctx):
+        main_id = kwargs.get('main_id')
+        user_id = kwargs.get('user_id')
+        user_address = kwargs.get('user_address')
+        
+        success = True
+        try:
+            # backdoor for testing timeout
+            if user_address == "timeout":
+                from time import sleep
+                sleep(40)
 
-        create_delivery(main_id=main_id, buyer_id=user_id, buyer_address=user_address)
-    except SoftTimeLimitExceeded as e:
-        print(e)
-        success = False
-        kwargs["error"] = "timeout"
-    except Exception as e:
-        print(e)
-        success = False
-        kwargs["error"] = str(e)
-    
-    result_object = {
-        "main_id": main_id,
-        "success": success,
-        "service_name": "delivery",
-        "payload": kwargs,
-    }
-    result_collector.send_task(
-        RESULT_TASK_NAME,
-        kwargs=result_object,
-        task_id=str(main_id)
-    )
+            create_delivery(main_id=main_id, buyer_id=user_id, buyer_address=user_address)
+        except SoftTimeLimitExceeded as e:
+            print(e)
+            success = False
+            kwargs["error"] = "timeout"
+        except Exception as e:
+            print(e)
+            success = False
+            kwargs["error"] = str(e)
+        
+        carrier = {}
+        TraceContextTextMapPropagator().inject(carrier)
+        header = {"traceparent": carrier["traceparent"]}
+        result_object = {
+            "main_id": main_id,
+            "success": success,
+            "service_name": "delivery",
+            "payload": kwargs,
+        }
+        result_collector.send_task(
+            RESULT_TASK_NAME,
+            kwargs=result_object,
+            task_id=str(main_id),
+            headers=header
+        )
 
 @app.task(name='wk-delivery.tasks.rollback')
 def rollback(**kwargs):
-    main_id = kwargs.get('main_id')
-    try:
-        set_delivery_status(main_id, StatusEnum.FAILED)
-    except Exception as e:
-        print(e)
-        kwargs["error"] = str(e)
+    celery.current_task.request.headers.get("traceparent")
+    tracer = trace.get_tracer(__name__)
+    ctx = propagate.extract(celery.current_task.request.headers)
+    with tracer.start_as_current_span("create_order", context=ctx):
+        main_id = kwargs.get('main_id')
+        try:
+            set_delivery_status(main_id, StatusEnum.FAILED)
+        except Exception as e:
+            print(e)
+            kwargs["error"] = str(e)
 
-    result_object = {
-        "main_id": main_id,
-        "success": False,
-        "service_name": "delivery",
-        "payload": kwargs,
-    }
-    result_collector.send_task(
-        RESULT_TASK_NAME,
-        kwargs=result_object,
-        task_id=str(main_id)
-    )
-    return False
+        carrier = {}
+        TraceContextTextMapPropagator().inject(carrier)
+        header = {"traceparent": carrier["traceparent"]}
+        result_object = {
+            "main_id": main_id,
+            "success": False,
+            "service_name": "delivery",
+            "payload": kwargs,
+        }
+        result_collector.send_task(
+            RESULT_TASK_NAME,
+            kwargs=result_object,
+            task_id=str(main_id),
+            headers=header
+        )
+        return False
